2cd0083fdbc74a482ba928aea3f65902d4fc6e57
OliWebDevO <oliver-vdb@hotmail.com>
2025-03-31 17:56:13 +0200

    css

--- app/api/workflows/onboarding/route.ts
+++ app/api/workflows/onboarding/route.ts
@@ -44,55 +44,33 @@ export const { POST } = serve<InitialData>(async (context) => {
     // await sendEmail({email, subject: "Welcome to the platform", message: `Hello ${fullName}, welcome to our platform!`})
     await sendEmail({email, subject: "Welcome to the platform", message: `<Html>
         <Section style={{ marginTop: 16, marginBottom: 16 }}>
-          <Img
-            alt="Herman Miller Chair"
-            height="320"
-            src="https://images.pexels.com/photos/1290141/pexels-photo-1290141.jpeg"
-            style={{
-              width: 100%;
-              border-radius: 12px;
-              object-fit: cover;
-            }}
+          <Img alt="Herman Miller Chair" height="320" src="https://images.pexels.com/photos/1290141/pexels-photo-1290141.jpeg"
+            style = "width: 100%; height: 320px; border-radius: 12px; object-fit: cover;"
           />
-          <Section
-            style={{
-              margin-top: 32px;
-              text-align: center;
-            }}
-          >
-            <p
-              style={{
-                margin-top: 16px;
-                margin-bottom: 16px;
-                font-size: 18px;
-                line-height: 28px;
-                font-weight: 600;
-                color: rgb(79,70,229);
-              }}
-            >
+          <Section style="margin-top: 32px; text-align: center;">
+            <p style = "margin-top: 16px; margin-bottom: 16px; font-size: 18px; line-height: 28px; font-weight: 600; color: rgb(79,70,229);">
               We are thrilled to have you here !
             </p>
             <h1
-              as="h1"
-              style={{
+              style="
                 margin: 0px;
                 margin-top: 8px;
                 font-size: 36px;
                 line-height: 36px;
                 font-weight: 600;
                 color: rgb(17,24,39);
-              }}
+              "
             >
               Welcome to our library  !
             </h1>
             <p
-              style={{ font-size: 16px; line-height: 24px; color: rgb(107,114,128); }}
+              style=" font-size: 16px; line-height: 24px; color: rgb(107,114,128); "
             >
              You are now a member of our platform ! You can reset your credential, move your books around, and never miss a beat !
             </p>
             <button
               href="https://university-library-v2-nu.vercel.app"
-              style={{
+              style="
                 margin-top: 16px;
                 border-radius: 8px;
                 background-color: rgb(79,70,229);
@@ -102,7 +80,7 @@ export const { POST } = serve<InitialData>(async (context) => {
                 padding-nottom: 12px;
                 font-weight: 600;
                 color: rgb(255,255,255);
-              }}
+              "
             >
               Visit Our Platform
             </button>
