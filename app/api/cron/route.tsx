33f3054e2a58daa825c4997434e697a1fdd63252
OliWebDevO <oliver-vdb@hotmail.com>
2025-06-24 20:18:03 +0200

    cronjob schedule to trigger database each day

--- app/api/cron/route.tsx
+++ app/api/cron/route.tsx
@@ -1,9 +1,12 @@
 import { NextResponse } from 'next/server';
 import { Redis } from '@upstash/redis';
 
-export const runtime = "nodejs";
+export async function GET(req: Request) {
+  // Optional: Protect with CRON_SECRET
+  if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
+    return new Response('Unauthorized', { status: 401 });
+  }
 
-export async function GET() {
   const redis = new Redis({
     url: process.env.UPSTASH_REDIS_REST_URL!,
     token: process.env.UPSTASH_REDIS_REST_TOKEN!,
